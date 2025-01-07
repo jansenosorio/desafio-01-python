5e1f76aa162840d1f4165ea98335cd5705fc6837
jansenosorio <contato.devosorio@gmail.com>
2025-01-07 10:40:49 -0300

    First Commit

---
+++ main.py
@@ -0,0 +1,16 @@
+def desafio1rocketseat():
+    print("Desafio 1 - Rocketseat")
+
+    print("Selecione uma das funções abaixo:")
+
+    print("[1] - Adicionar um novo contato")
+    print("[2] - Visualizar lista de contatos")
+    print("[3] - Editar um contato")
+    print("[4] - Favoritar/Desfavoritar um contato")
+    print("[5] - Lista de contatos favoritos")
+    print("[5] - Deletar um contato")
+
+    opcaoSelecionada = int(input("Digite a opção desejada: "))
+    print(f"Opção selecionada: {opcaoSelecionada}")
+
+desafio1rocketseat()
